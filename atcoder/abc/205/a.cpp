a76863e852419939cd641cf7c958d724f94192b1
fura2 <yamazoe0577@gmail.com>
2021-06-14 21:30:06 +0900

    add abc205-a

---
+++ atcoder/abc/205/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+#define rep(i,n) for(int i=0;i<(n);i++)
+
+using namespace std;
+
+int main(){
+	int a,b; scanf("%d%d",&a,&b);
+	printf("%.9f\n",a*b/100.0);
+	return 0;
+}
