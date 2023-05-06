56ea19ba9992f1b69d1550efa6a09a803667c8c7
fura2 <yamazoe0577@gmail.com>
2023-05-06 22:00:51 +0900

    add arc105-e

--- atcoder/arc/105/e.cpp
+++ atcoder/arc/105/e.cpp
@@ -3,6 +3,7 @@
 #define rep(i,n) for(int i=0;i<(n);i++)
 
 using namespace std;
+using lint=long long;
 
 using graph=vector<vector<int>>;
 
@@ -31,73 +32,37 @@ vector<vector<int>> connected_components(const graph& G){
 	return res;
 }
 
-bool dfs(int turn,int a,int b,vector<int> c){
-	if(c.empty()){
-		if(turn==0) return !(a<2 && b<2) && !(2<=a && 2<=b);
-		else        return  (a<2 && b<2) ||  (2<=a && 2<=b);
-	}
-
-	rep(i,c.size()){
-		int a2,b2;
-		vector<int> c2;
-
-		// c[i] を a にマージ
-		a2=(a+c[i])%4; b2=b;
-		c2=c;
-		c2.erase(c2.begin()+i);
-		if(!dfs(1-turn,a2,b2,c2)) return true;
-
-		// c[i] を b にマージ
-		a2=a; b2=(b+c[i])%4;
-		c2=c;
-		c2.erase(c2.begin()+i);
-		if(!dfs(1-turn,a2,b2,c2)) return true;
-
-		for(int j=i+1;j<c.size();j++){
-			// c[i] と c[j] をマージ
-			a2=a; b2=b;
-			c2={(c[i]+c[j])%4};
-			rep(k,c.size()) if(k!=i && k!=j) c2.emplace_back(c[k]);
-			if(!dfs(1-turn,a2,b2,c2)) return true;
-		}
-	}
-	return false;
-}
-
 void solve(){
-	int n,m; scanf("%d%d",&n,&m);
+	lint n,m; scanf("%lld%lld",&n,&m);
 	graph G(n);
 	rep(i,m){
 		int u,v; scanf("%d%d",&u,&v); u--; v--;
 		add_undirected_edge(G,u,v);
 	}
 
-	auto CC=connected_components(G);
-
-	int a,b,c[4]={};
-	for(auto C:CC){
-		bool has_a=false,has_b=false;
-		for(int u:C){
-			if(u== 0 ) has_a=true;
-			if(u==n-1) has_b=true;
-		}
-		if     (has_a) a=C.size()%4;
-		else if(has_b) b=C.size()%4;
-		else           c[C.size()%4]++;
+	bool res;
+	if(n%2==1){
+		res=(n*(n-1)/2-m)&1;
 	}
-	int mn=min(c[1],c[3]);
-	c[1]-=mn;
-	c[3]-=mn;
-	c[1]%=4;
-	c[3]%=4;
-	c[2]%=2;
-
-	vector<int> t;
-	for(int x=1;x<=3;x++) rep(_,c[x]) t.emplace_back(x);
-	puts(dfs(m%2,a,b,t)?"First":"Second");
+	else{
+		lint a,b;
+		for(auto C:connected_components(G)){
+			for(int u:C){
+				if     (u== 0 ) a=C.size();
+				else if(u==n-1) b=C.size();
+			}
+		}
+		if((a&1)!=(b&1)){
+			res=true;
+		}
+		else{
+			res=(n*(n-1)/2-a*b-m)&1;
+		}
+	}
+	puts(res?"First":"Second");
 }
 
 int main(){
-	int q; scanf("%d",&q); rep(_,q) solve();
+	int t; scanf("%d",&t); rep(_,t) solve();
 	return 0;
 }
