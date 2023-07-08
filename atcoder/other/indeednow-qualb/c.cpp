1f0febb5f838b0ef4801e1c71c0a1b5da9a5bc47
fura2 <yamazoe0577@gmail.com>
2023-07-08 16:42:44 +0900

    add indeednow-qualb a-c

---
+++ atcoder/other/indeednow-qualb/c.cpp
@@ -0,0 +1,136 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#define _overload4(a, b, c, d, e, ...) e
+#define _overload5(a, b, c, d, e, f, ...) f
+#define _rep4(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define _rep3(i, a, b) _rep4(i, a, b, 1)
+#define _rep2(i, n) _rep3(i, 0, n)
+#define _rep1(n) for (int _ = 0; _ < (n); ++_)
+#define _rrep4(i, a, b, c) for (int i = (b) - 1; i >= (a); i -= (c))
+#define _rrep3(i, a, b) _rrep4(i, a, b, 1)
+#define _rrep2(i, n) _rrep3(i, 0, n)
+
+#define rep(...) _overload4(__VA_ARGS__, _rep4, _rep3, _rep2, _rep1)(__VA_ARGS__)
+#define rrep(...) _overload4(__VA_ARGS__, _rrep4, _rrep3, _rrep2)(__VA_ARGS__)
+#define all(v) (v).begin(), (v).end()
+
+#define _show1(a) { std::clog << #a << " = " << a << endl; }
+#define _show2(a, ...) { std::clog << #a << " = " << a << ", "; _show1(__VA_ARGS__); }
+#define _show3(a, ...) { std::clog << #a << " = " << a << ", "; _show2(__VA_ARGS__); }
+#define _show4(a, ...) { std::clog << #a << " = " << a << ", "; _show3(__VA_ARGS__); }
+#define _show5(a, ...) { std::clog << #a << " = " << a << ", "; _show4(__VA_ARGS__); }
+#define show(...) _overload5(__VA_ARGS__, _show5, _show4, _show3, _show2, _show1)(__VA_ARGS__)
+
+template<typename T>
+constexpr inline int bit(T x, unsigned int k) { return (x >> k) & 1; }
+
+template<typename C>
+long ssize(const C& c) { return std::size(c); }  // workaround until C++20
+
+void yes() { std::cout << "Yes\n"; }
+void no() { std::cout << "No\n"; }
+void yesno(bool b) { std::cout << (b ? "Yes" : "No") << "\n"; }
+
+template<class T, class U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::set<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& e : s) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "}";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::unordered_set<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& e : s) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "}";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::multiset<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& e : s) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "}";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    os << "[";
+    bool first = true;
+    for (const auto& e : v) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "]";
+    return os;
+}
+
+struct Setup {
+    Setup() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(11);
+    }
+} _setup;
+
+using namespace std;
+
+using graph = vector<vector<int>>;
+
+void add_undirected_edge(graph& G, int u, int v) {
+    G[u].emplace_back(v);
+    G[v].emplace_back(u);
+}
+
+int main() {
+    int n; cin >> n;
+    graph T(n);
+    rep(i, n - 1) {
+        int u, v; cin >> u >> v; u--; v--;
+        add_undirected_edge(T, u, v);
+    }
+
+    vector<bool> vis(n);
+    priority_queue<int, vector<int>, greater<>> Q;
+    vis[0] = true;
+    Q.emplace(0);
+    vector<int> ans;
+    while (!Q.empty()) {
+        int u = Q.top(); Q.pop();
+        ans.emplace_back(u);
+        for (int v : T[u]) if (!vis[v]) {
+            vis[v] = true;
+            Q.emplace(v);
+        }
+    }
+    rep(i, n) cout << ans[i] + 1 << " \n"[i == n - 1];
+
+    return 0;
+}
