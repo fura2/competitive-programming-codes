70dd0d329813dd08568e71344ceff4a6b82cd8d0
fura2 <yamazoe0577@gmail.com>
2023-07-10 02:46:30 +0900

    add arc164 d

---
+++ atcoder/arc/164/d.cpp
@@ -0,0 +1,182 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#define _overload4(a, b, c, d, e, ...) e
+#define _overload5(a, b, c, d, e, f, ...) f
+#define _overload6(a, b, c, d, e, f, g, ...) g
+
+#define _rep4(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define _rep3(i, a, b) _rep4(i, a, b, 1)
+#define _rep2(i, n) _rep3(i, 0, n)
+#define _rep1(n) for (int _ = 0; _ < (n); ++_)
+#define rep(...) _overload4(__VA_ARGS__, _rep4, _rep3, _rep2, _rep1)(__VA_ARGS__)
+
+#define _rrep4(i, a, b, c) for (int i = (b) - 1; i >= (a); i -= (c))
+#define _rrep3(i, a, b) _rrep4(i, a, b, 1)
+#define _rrep2(i, n) _rrep3(i, 0, n)
+#define rrep(...) _overload4(__VA_ARGS__, _rrep4, _rrep3, _rrep2)(__VA_ARGS__)
+
+#define all(v) begin(v), end(v)
+
+#define _show1(a) { std::clog << #a << " = " << a << endl; }
+#define _show2(a, ...) { std::clog << #a << " = " << a << ", "; _show1(__VA_ARGS__); }
+#define _show3(a, ...) { std::clog << #a << " = " << a << ", "; _show2(__VA_ARGS__); }
+#define _show4(a, ...) { std::clog << #a << " = " << a << ", "; _show3(__VA_ARGS__); }
+#define _show5(a, ...) { std::clog << #a << " = " << a << ", "; _show4(__VA_ARGS__); }
+#define _show6(a, ...) { std::clog << #a << " = " << a << ", "; _show5(__VA_ARGS__); }
+#define show(...) _overload6(__VA_ARGS__, _show6, _show5, _show4, _show3, _show2, _show1)(__VA_ARGS__)
+
+template<typename T>
+constexpr inline int bit(T x, unsigned int k) { return (x >> k) & 1; }
+
+template<typename C>
+long ssize(const C& c) { return std::size(c); }  // workaround until C++20
+
+void yes() { std::cout << "Yes\n"; }
+void no() { std::cout << "No\n"; }
+void yesno(bool b) { std::cout << (b ? "Yes" : "No") << "\n"; }
+
+template<class T, class U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::set<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& e : s) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "}";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::unordered_set<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& e : s) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "}";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::multiset<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& e : s) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "}";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    os << "[";
+    bool first = true;
+    for (const auto& e : v) {
+        if (!first) os << ", ";
+        first = false;
+        os << e;
+    }
+    os << "]";
+    return os;
+}
+
+struct Setup {
+    Setup() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(11);
+    }
+} _setup;
+
+using namespace std;
+
+class mint {
+    static const int MOD = 998244353;
+    int x;
+public:
+    mint():x(0) {}
+    mint(long long y) { x = y % MOD; if (x < 0) x += MOD; }
+
+    mint& operator+=(const mint& m) { x += m.x; if (x >= MOD) x -= MOD; return *this; }
+    mint& operator-=(const mint& m) { x -= m.x; if (x < 0) x += MOD; return *this; }
+    mint& operator*=(const mint& m) { x = (long long)x * m.x % MOD; return *this; }
+    mint& operator/=(const mint& m) { return *this *= inverse(m); }
+    mint operator+(const mint& m)const { return mint(*this) += m; }
+    mint operator-(const mint& m)const { return mint(*this) -= m; }
+    mint operator*(const mint& m)const { return mint(*this) *= m; }
+    mint operator/(const mint& m)const { return mint(*this) /= m; }
+    mint operator-()const { return -x; }
+
+    friend mint inverse(const mint& m) {
+        int a = m.x, b = MOD, u = 1, v = 0;
+        while (b > 0) { int t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); }
+        return u;
+    }
+
+    friend istream& operator>>(istream& is, mint& m) { long long t; is >> t; m = t; return is; }
+    friend ostream& operator<<(ostream& os, const mint& m) { return os << m.x; }
+    int to_int()const { return x; }
+};
+
+mint operator+(long long x, const mint& m) { return mint(x) + m; }
+mint operator-(long long x, const mint& m) { return mint(x) - m; }
+mint operator*(long long x, const mint& m) { return mint(x) * m; }
+mint operator/(long long x, const mint& m) { return mint(x) / m; }
+
+int main() {
+    int n;
+    string s; cin >> n >> s;
+
+    int offset = n;
+    // dp1[i][j] = (s[0, i) の ? への +/- の割り当て方であって Σ_{0=k<i}s_k = j を満たすものの個数)
+    //              ただし、+/- をそれぞれ +1/-1 とカウントする
+    vector dp1(2 * n + 1, vector(2 * n + 1, mint(0)));
+    dp1[0][offset] = 1;
+    rep(i, 2 * n) {
+        if (s[i] != '-') { // '+' を置く
+            rep(j, -n, n) {
+                dp1[i + 1][j + 1 + offset] += dp1[i][j + offset];
+            }
+        }
+        if (s[i] != '+') { // '-' を置く
+            rep(j, -(n - 1), n + 1) {
+                dp1[i + 1][j - 1 + offset] += dp1[i][j + offset];
+            }
+        }
+    }
+
+    // dp2[i][j] = (s[0, i) の ? への +/- の割り当て方であって Σ_{0=k<i}s_k = j を満たすものすべてに対する
+    //              1 <= x <= i からの(電荷の移動距離の)寄与の総和)
+    vector dp2(2 * n + 1, vector(2 * n + 1, mint(0)));
+    rep(i, 2 * n) {
+        if (s[i] != '-') { // '+' を置く
+            rep(j, -n, n) {
+                dp2[i + 1][j + 1 + offset] += dp2[i][j + offset] + abs(j + 1) * dp1[i][j + offset];
+            }
+        }
+        if (s[i] != '+') { // '-' を置く
+            rep(j, -(n - 1), n + 1) {
+                dp2[i + 1][j - 1 + offset] += dp2[i][j + offset] + abs(j - 1) * dp1[i][j + offset];
+            }
+        }
+    }
+    cout << dp2.back()[offset] << endl;
+
+    return 0;
+}
