2b7be32183f263d13d6c5ce16036a5af8df5eae8
fura2 <yamazoe0577@gmail.com>
2023-09-11 22:44:46 +0900

    add yukicoder 2466

---
+++ yukicoder/2466.cpp
@@ -0,0 +1,15 @@
+#include "template.hpp"
+
+int main() {
+    int n;
+    cin >> n;
+    // rep (k, 1, 20) {
+    //     double x = sqrt(1);
+    //     rrep (i, k - 1) {
+    //         x = sqrt(1 + (n + i) * x);
+    //     }
+    //     cout << k << ": " << x << endl;
+    // }
+    cout << n + 1 << endl;
+    return 0;
+}
