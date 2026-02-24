c294dfb7f206dd83820cb7ef0aa471ee4e38df1b
fura2 <yamazoe0577@gmail.com>
2026-02-25 03:07:39 +0900

    add another solution for ABC446 G

---
+++ atcoder/abc/446/g2.cpp
@@ -0,0 +1,205 @@
+// solution based on https://atcoder.jp/contests/abc446/editorial/16429
+// 解説とは遷移の式が少し違う
+
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#define impl_overload4(a, b, c, d, e, ...) e
+#define impl_overload5(a, b, c, d, e, f, ...) f
+#define impl_overload6(a, b, c, d, e, f, g, ...) g
+#define impl_overload7(a, b, c, d, e, f, g, h, ...) h
+
+// clang-format off
+#define impl_rep4(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define impl_rep3(i, a, b) impl_rep4(i, a, b, 1)
+#define impl_rep2(i, n) impl_rep3(i, 0, n)
+#define impl_rep1(n) for (int _ = 0; _ < (n); ++_)
+#define rep(...) impl_overload4(__VA_ARGS__, impl_rep4, impl_rep3, impl_rep2, impl_rep1)(__VA_ARGS__)
+
+#define impl_rrep4(i, a, b, c) for (int i = (b) - 1; i >= (a); i -= (c))
+#define impl_rrep3(i, a, b) impl_rrep4(i, a, b, 1)
+#define impl_rrep2(i, n) impl_rrep3(i, 0, n)
+#define rrep(...) impl_overload4(__VA_ARGS__, impl_rrep4, impl_rrep3, impl_rrep2)(__VA_ARGS__)
+// clang-format on
+
+#define all(v) std::begin(v), std::end(v)
+#define rall(v) std::rbegin(v), std::rend(v)
+
+template<typename T>
+constexpr int bit(T x, unsigned int k) {
+  return (x >> k) & 1;
+}
+
+template<typename T>
+constexpr bool chmax(T& a, const T& b) {
+  return a < b ? a = b, true : false;
+}
+template<typename T>
+constexpr bool chmin(T& a, const T& b) {
+  return a > b ? a = b, true : false;
+}
+
+void yesno(bool b) {
+  std::cout << (b ? "Yes" : "No") << "\n";
+}
+void yes() {
+  yesno(true);
+}
+void no() {
+  yesno(false);
+}
+
+struct Setup {
+  Setup() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout << std::fixed << std::setprecision(11);
+  }
+} setup;
+
+#ifdef LOCAL
+#include "template_local.hpp"
+#else
+#define show(...) ((void)0)
+#endif
+
+using uint = unsigned int;
+using lint = long long;
+using ulint = unsigned long long;
+
+using namespace std;
+
+#endif // TEMPLATE_HPP
+
+class mint {
+  static const int MOD = 998244353;
+  int x;
+
+ public:
+  mint(): x(0) {}
+  mint(long long y) {
+    x = y % MOD;
+    if (x < 0) x += MOD;
+  }
+
+  mint& operator+=(const mint& m) {
+    x += m.x;
+    if (x >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint& m) {
+    x -= m.x;
+    if (x < 0) x += MOD;
+    return *this;
+  }
+  mint& operator*=(const mint& m) {
+    x = (long long)x * m.x % MOD;
+    return *this;
+  }
+  mint& operator/=(const mint& m) {
+    return *this *= inverse(m);
+  }
+  mint operator+(const mint& m) const {
+    return mint(*this) += m;
+  }
+  mint operator-(const mint& m) const {
+    return mint(*this) -= m;
+  }
+  mint operator*(const mint& m) const {
+    return mint(*this) *= m;
+  }
+  mint operator/(const mint& m) const {
+    return mint(*this) /= m;
+  }
+  mint operator-() const {
+    return -x;
+  }
+
+  friend mint inverse(const mint& m) {
+    int a = m.x, b = MOD, u = 1, v = 0;
+    while (b > 0) {
+      int t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    return u;
+  }
+
+  friend istream& operator>>(istream& is, mint& m) {
+    long long t;
+    is >> t;
+    m = t;
+    return is;
+  }
+  friend ostream& operator<<(ostream& os, const mint& m) {
+    return os << m.x;
+  }
+  int to_int() const {
+    return x;
+  }
+};
+
+mint operator+(long long x, const mint& m) {
+  return mint(x) + m;
+}
+mint operator-(long long x, const mint& m) {
+  return mint(x) - m;
+}
+mint operator*(long long x, const mint& m) {
+  return mint(x) * m;
+}
+mint operator/(long long x, const mint& m) {
+  return mint(x) / m;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep (i, n) cin >> a[i];
+
+  vector pos(n + 1, vector<int>());
+  rep (i, n) pos[a[i]].emplace_back(i);
+
+  // dp[i][c] = (a[0..i) の 221 部分列であって、末尾が c であるようなものの個数)
+  // ただし、dp 配列は i についてインラインに更新する
+  vector<mint> dp(n + 1);
+  // sum[i] = (a[0..i) の 221 部分列の個数)
+  //        = 1 + Σ_c dp[i][c] (1 は空列の分)
+  vector<mint> sum(n + 1, 1);
+  vector<mint> mem(n + 1); // mem[i + 1] = dp[i + 1][a[i]]
+  rep (i, n) {
+    int c = a[i];
+    int k = lower_bound(all(pos[c]), i) - pos[c].begin();
+
+    mint dp_prev = dp[c];
+    if (k + 1 < c) { // c が c 個未満
+      dp[c] = 0;
+    }
+    else if (k + 1 == c) { // ちょうど c 回目の c
+      // j = (i, i-1, i-2, ... と見ていったときに c 回目に現れる c の位置)
+      int j = pos[c][k - c + 1];
+      dp[c] = sum[j];
+    }
+    else { // k + 1 > c
+      // j = (i, i-1, i-2, ... と見ていったときに c 回目に現れる c の位置)
+      int j = pos[c][k - c + 1];
+      // j2 = (i, i-1, i-2, ... と見ていったときに c + 1 回目に現れる c の位置)
+      int j2 = pos[c][k - c];
+      dp[c] = sum[j] - mem[j2 + 1];
+    }
+
+    sum[i + 1] = sum[i] + dp[c] - dp_prev;
+    mem[i + 1] = dp[c];
+  }
+  cout << sum[n] - 1 << endl;
+
+  return 0;
+}
